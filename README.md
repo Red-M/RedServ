52bd025db85bcfd1e60497b99c7bd2205a27b429
Red-M <pooatyou.com@gmail.com>
2014-04-24 20:21:40 +1000

    README update, added example sieve-in and sieve-out files. webserver.py would generate the pages folder anyway if it wasn't there already. Doesn't matter so much now but the sieve files need to be in ./pages

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 RedServ
 =======
 
-RedServ
+RedServ is a web server built on top of Cherrypy and uses Mako for the template rendering.
+
+Requirements:
+- Python 2.7+
+- Cherrypy3
+- Mako
+
+Installing:
+1. Install Python 2.7 or higher
+2. Install Cherrypy or Cherrypy3 on ubuntu (python-cherrypy or python-cherrypy3)
+3. Install mako (python-mako)
+4. git clone https://github.com/Red-M/RedServ.git
+5. Run ./webserver.py
+6. Configure the setting via ./config (config file to be added sometime soon)
+7. run ./webserver.py
+8. Drop web site files into ./pages
+
+opitional:
+- Add SSL certs and edit ./config to turn on and to make sure the certs are named the same as in the config
+
+
+RedServ's main purpose is to provide an Apache2 + PHP like experience but with just pure Python.
+The main web server file is very small and doesn't require as much work to get going.
+The Python scripting will be documented but there will be examples provided for how everything works, for now have fun playing with it.
+
+-Red_M
---
+++ pages/sieve-out.py
@@ -0,0 +1,23 @@
+global regexmatch2
+def regexmatch2(text,regex):
+    match = regex.match(text)
+    if not match:
+        return("")
+    (matcho,) = match.groups()
+    return matcho
+
+def sieve(params,datareturned,cj,headers,response,request):
+    virthost = request.headers["host"]
+    if os.name == "posix":
+        datareturned = str(datareturned).replace(os.path.abspath('pages')+"/"+virthost,"")
+        datareturned = str(datareturned).replace(os.path.abspath('pages'),"")
+        datareturned = str(datareturned).replace(str(os.path.abspath('pages')).replace("/pages",""),"")
+    if os.name == "nt":
+        datareturned = str(datareturned).replace(os.path.abspath('pages')+"\\"+virthost,"")
+        datareturned = str(datareturned).replace(os.path.abspath('pages'),"")
+        datareturned = str(datareturned).replace(str(os.path.abspath('pages')).replace("\\pages",""),"")
+    datareturned = str(datareturned).replace("4dff0fdd8d51b65f9adcbaf0a3c84319","")
+    datareturned = datareturned + debughandler(params)
+    return(params,datareturned,cj,headers,response,request)
+    
+params,datareturned,cj,headers,response,request = sieve(params,datareturned,cj,headers,response,request)
