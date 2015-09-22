ebbbb80b6ddd5c69c7d726b9a359d88fab6f4ef8
Red_M <pooatyou.com@gmail.com>
2015-09-22 21:14:30 +1000

    Small edit to allow for a top level param in a request to be a list. This allows for the previous bug to be fixed and still allow some functionality while keeping performance loss to a minimum.

--- webserver.py
+++ webserver.py
@@ -645,7 +645,11 @@ class WebInterface:
         paramlines = "?"
         if not params=={}:
             for data in params:
-                params[data] = str(params[data]).replace("\n","\\n").replace("\r","\\r")
+                if isinstance(params[data],type([])):
+                    for list_data in params[data]:
+                        params[data][list_data] = params[data][list_data].replace("\n","\\n").replace("\r","\\r")
+                else:
+                    params[data] = str(params[data]).replace("\n","\\n").replace("\r","\\r")
                 paramlines = paramlines+data+"="+params[data]+"&"
             paramlines = paramlines[:-1]
         if paramlines=="?":
