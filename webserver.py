773b6e61f83e29ec24c58838a041224ad9d1a444
Red_M <pooatyou.com@gmail.com>
2015-09-22 21:26:01 +1000

    Speed up for param lists.

--- webserver.py
+++ webserver.py
@@ -646,8 +646,7 @@ class WebInterface:
         if not params=={}:
             for data in params:
                 if isinstance(params[data],type([])):
-                    for list_data in params[data]:
-                        params[data][list_data] = params[data][list_data].replace("\n","\\n").replace("\r","\\r")
+                    params[data] = [params[data][list_data].replace("\n","\\n").replace("\r","\\r") for list_data in params[data]]
                 else:
                     params[data] = str(params[data]).replace("\n","\\n").replace("\r","\\r")
                 paramlines = paramlines+data+"="+params[data]+"&"
